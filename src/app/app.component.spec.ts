cbe13254b34cf36ef684fb076f08d5930815348f
William Tyler Pelt <42362488+wtpelt@users.noreply.github.com>
2022-05-06 19:46:37 -0400

    Updated Angular from 9 to 13; Removed a featured website publication and added another; Added a dynamic copyright date.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dm-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('dm-app');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dm-app!');
-  });
 });
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class FooterComponent {
+  public copyrightYear = new Date().getFullYear();
 }
